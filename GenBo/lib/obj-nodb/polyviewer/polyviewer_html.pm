6355bfa3b04c3b2890d149665f9097b1a45378d0
mbras <mbras@sanger.cm.cluster>
2022-03-28 14:56:26 +0200

    bug '-' alamut

--- GenBo/lib/obj-nodb/polyviewer/polyviewer_html.pm
+++ GenBo/lib/obj-nodb/polyviewer/polyviewer_html.pm
@@ -556,7 +556,7 @@ sub alamut {
 	my $start = $self->variant->start;
 	my $a0 = $self->variant->ref_allele;
 	my $a1 = $self->variant->allele;
-	my $chr_name = $self->variant->chromosome."-";
+	my $chr_name = $self->variant->chromosome;
 	my $qq5 = qq{	<button    class="alamutView3" onClick ="displayInAlamut('$chr_name',$start,['$a0','$a1']);"></button>};
 	return $qq5;
 #	die();
