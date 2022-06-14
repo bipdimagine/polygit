4329703d91c1cc5c8182b630c39b2705347c10ce
mbras <mbras@sanger.cm.cluster>
2022-06-14 10:39:13 +0200

    add authentification login/pwd in RNA analyse

--- polymorphism-cgi/json_output_nodb/projects_json_rna_analysis.pl
+++ polymorphism-cgi/json_output_nodb/projects_json_rna_analysis.pl
@@ -52,15 +52,14 @@ my $only_diag = $cgi->param('only_diag');
 my $export_xls = $cgi->param('export_xls');
 my $viewer = $cgi->param('viewer');
 my $defidiag = $cgi->param('defidiag');
-getProjectListsRNA($buffer,$login,$pwd) if $action eq "list" && !($project_query) ;
-checkAuthentification($buffer,$login,$pwd,$cgi->param('project')) if $action eq "check";
+getProjectListsRNA($buffer,$login,$pwd,$project_query) if $action eq "list";
 
 
  
 sub getProjectListsRNA {
-	my ( $buffer, $login, $pwd ) = @_;
+	my ( $buffer, $login, $pwd, $project_name ) = @_;
 	my $is_BIPD_login = $buffer->getQuery->isLoginSTAFF($login);
-	my $list_proj = $buffer->getQuery->getListProjectsRnaSeq();
+	my $list_proj = $buffer->getQuery->getListProjectsRnaSeq($project_name);
 	my @list_res;
 	foreach my $h (@$list_proj) {
 		my $h_users;
