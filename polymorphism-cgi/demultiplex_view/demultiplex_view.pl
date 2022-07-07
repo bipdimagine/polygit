bea3836f31df037005840e1fe786715a43d6e7e5
mbras <mbras@sanger.cm.cluster>
2022-07-07 19:09:35 +0200

    update demultiplex view run DRAGEN

--- polymorphism-cgi/demultiplex_view/demultiplex_view.pl
+++ polymorphism-cgi/demultiplex_view/demultiplex_view.pl
@@ -49,14 +49,14 @@ foreach my $this_path (@$list_paths_found) {
 			next if ($name =~ /run1[0-9]+/);
 			add_file_json('https://www.polyweb.fr/polyweb/demultiplex_view/demultiplex_view_run.html?name='.$name.'&json='.$json_file, $json_path, $name);
 		}
-		elsif (-e $this_path.'/Demultiplex_Stats.csv') {
-			convert_csv_to_html($this_path.'/Demultiplex_Stats.csv', $this_path.'/html/laneBarcode.html') if (not -e $this_path.'/html/laneBarcode.html' || $force);
-			add_file_html($this_path.'/html/laneBarcode.html', 'laneBarcode.html');
-		}
-		elsif (-e $this_path.'/Top_Unknown_Barcodes.csv') {
-			convert_csv_to_html($this_path.'/Top_Unknown_Barcodes.csv', $this_path.'/html/top_unknown_barcodes.html') if (not -e $this_path.'/html/top_unknown_barcodes.html' || $force);
-			add_file_html($this_path.'/html/top_unknown_barcodes.html', 'top_unknown_barcodes.html');
-		}
+#		elsif (-e $this_path.'/Demultiplex_Stats.csv') {
+#			convert_csv_to_html($this_path.'/Demultiplex_Stats.csv', $this_path.'/html/laneBarcode.html') if (not -e $this_path.'/html/laneBarcode.html' || $force);
+#			add_file_html($this_path.'/html/laneBarcode.html', 'laneBarcode.html');
+#		}
+#		elsif (-e $this_path.'/Top_Unknown_Barcodes.csv') {
+#			convert_csv_to_html($this_path.'/Top_Unknown_Barcodes.csv', $this_path.'/html/top_unknown_barcodes.html') if (not -e $this_path.'/html/top_unknown_barcodes.html' || $force);
+#			add_file_html($this_path.'/html/top_unknown_barcodes.html', 'top_unknown_barcodes.html');
+#		}
 		
 		
 	}
@@ -165,7 +165,10 @@ sub check_run_id_from_sample_name {
 sub convert_csv_to_json {
 	my ($path, $list_files) = @_;
 	my $path_out = $path.'/json/';
-	mkdir $path_out unless (-d $path_out);
+	unless (-d $path_out) {
+		mkdir $path_out;
+		`chmod 777 $path_out`;
+	}
 	my $file_out = "$path_out/demultiplex.json";
 	return $file_out if (-e $file_out and not $force);
 	my $h;
@@ -182,6 +185,8 @@ sub convert_csv_to_json {
 		my $i = 0;
 		my $has_RC;
 		my $max_nb_header;
+		my $nb_samples;
+		my $total_reads;
 		while (<CSV>) {
 			chomp($_);
 			my $line = $_;
@@ -204,6 +209,7 @@ sub convert_csv_to_json {
 					$h->{$file}->{values}->{$id}->{$j} = $value;
 					if ($cat eq 'SampleID') {
 						$sample_id = $value;
+						$nb_samples++ if (not lc($sample_id) eq 'undetermined' and not lc($sample_id) =~ /_rc/);
 						my ($run_id, @l_runs_id);
 						if (exists $h->{$file}->{runs_ids} and exists $h->{$file}->{runs_ids}->{$value}) {
 							@l_runs_id = sort keys %{$h->{$file}->{runs_ids}->{$value}};
@@ -212,26 +218,24 @@ sub convert_csv_to_json {
 							my $h_runs_infos = check_run_id_from_sample_name($value) if (lc($value) ne 'undetermined');
 							@l_runs_id = sort keys %{$h_runs_infos};
 							foreach my $r (@l_runs_id) {
-								$h->{$file}->{runs_ids}->{$value}->{$r} = undef;
-								$h->{$file}->{runs_ids}->{$r}->{$value} = undef;
+								$h->{$file}->{runs_ids}->{by_samples}->{$value}->{$r} = undef;
+								$h->{$file}->{runs_ids}->{by_runs}->{$r}->{$value} = undef;
 							}
 						}
 						my $runs = join(' ', reverse @l_runs_id);
 						$runs = 'no_run_info' unless ($runs);
 						$h->{$file}->{values}->{$id}->{'RunID'} = $runs;
 						$h->{$file}->{values}->{$id}->{$max_nb_header} = $runs;
-						#$h->{$file}->{values}->{$id}->{'RunID'} = $run_id;
-						#$h->{$file}->{values}->{$id}->{$max_nb_header} = $run_id;
 					}
 					if ($cat eq '# Reads') {
+						$total_reads += int($value)  if (not $sample_id =~ /undetermined/ and not $sample_id =~ /_RC$/);
 						$h->{$file}->{values}->{$id}->{'# Reads Norm Only'} = int($value);
 						$h->{$file}->{values}->{$id}->{$max_nb_header+1} = int($value);
-						#TODO: prendre les valeurs par RUN et Patient pour faire les ratio
-#						if (exists $h->{$file}->{runs_ids} and exists $h->{$file}->{runs_ids}->{$sample_id}) {
-#							foreach my $run_id (keys %{$h->{$file}->{runs_ids}->{$value}}) {
-#								
-#							}
-#						}
+						if (exists $h->{$file}->{runs_ids}->{by_samples}->{$sample_id}) {
+							foreach my $run_id (keys %{$h->{$file}->{runs_ids}->{by_samples}->{$sample_id}}) {
+								$h->{$file}->{runs_ids}->{by_samples}->{$sample_id}->{$run_id} = int($value);
+							}
+						}
 					}
 					
 					$j++;
@@ -240,9 +244,24 @@ sub convert_csv_to_json {
 			$i++;
 		}
 		close(CSV);
+		
+		my ($is_demultiplex_file, $is_top_barcodes_file);
+		$is_demultiplex_file = 1 if (lc($file) =~ /demultiplex/);
+		$is_top_barcodes_file = 1 if (lc($file) =~ /top_/);
 
-		if (lc($file) =~ /demultiplex/) {
-			
+		if ($is_demultiplex_file) {
+			foreach my $run_id (keys %{$h->{$file}->{runs_ids}->{by_runs}}) {
+				my @lSamples = keys %{$h->{$file}->{runs_ids}->{by_runs}->{$run_id}};
+				my $nb_pat = 0;
+				my $reads_total = 0;
+				foreach my $sample (@lSamples) {
+					next if (lc($sample) =~ /undetermined/);
+					$nb_pat++;
+					$reads_total += $h->{$file}->{runs_ids}->{by_samples}->{$sample}->{$run_id};
+				}
+				$h->{$file}->{runs_ids}->{by_runs}->{$run_id}->{nb_samples} = $nb_pat;
+				$h->{$file}->{runs_ids}->{by_runs}->{$run_id}->{mean_reads} = $reads_total / $nb_pat;
+			}
 			push(@{$h->{$file}->{header}}, 'RunID');
 		}
 		
@@ -250,8 +269,9 @@ sub convert_csv_to_json {
 			$sorted_col = "data-sort-name='n_reads_norm_only' data-sort-order='desc'";
 			push(@{$h->{$file}->{header}}, '# Reads Norm Only');
 		}
+		push(@{$h->{$file}->{header}}, 'Seems Ok') if ($is_demultiplex_file);
 		
-		my $html = qq{<table style="width:100%;" $sorted_col data-filter-control='true'data-toggle="table" data-show-extended-pagination="true" data-cache="false" data-pagination-loop="false" data-virtual-scroll="true" data-pagination-pre-text="Previous" data-pagination-next-text="Next" data-pagination="true" data-page-size="15" data-page-list="[10, 15, 20,30, 50, 100, 200, 300]" data-resizable='true' id='$table_id' class='table table-striped sortable-table' style='font-size:13px;'>};
+		my $html = qq{<table style="width:100%;" $sorted_col data-filter-control='true'data-toggle="table" data-show-extended-pagination="true" data-cache="false" data-pagination-loop="false" data-virtual-scroll="true" data-pagination-pre-text="Previous" data-pagination-next-text="Next" data-pagination="true" data-page-size="10" data-page-list="[10, 15, 20,30, 50, 100, 200, 300]" data-resizable='true' id='$table_id' class='table table-striped sortable-table' style='font-size:13px;'>};
 		$html .= "<thead>\n";
 		foreach my $cat (@{$h->{$file}->{header}}) {
 			my $cat_name = $cat;
@@ -285,7 +305,7 @@ sub convert_csv_to_json {
 					
 				}
 			}
-			elsif ($cat eq 'RunID') { $sortable = qq{ data-sortable='true' data-filter-control='select' }; }
+			elsif ($cat eq 'RunID' or $cat eq 'Seems Ok') { $sortable = qq{ data-sortable='true' data-filter-control='select' }; }
 			else { $sortable = qq{ data-sortable='true' data-filter-control='input' }; }
 			$html .= "<th $sortable data-field='".lc($cat_name)."' ><center><b>$cat</b></center></th>\n";
 		}
@@ -294,11 +314,31 @@ sub convert_csv_to_json {
 		$html .= "<tbody>\n";
 		
 		my $nb_values = scalar(keys %{$h->{$file}->{values}});
-		my $limit_errors_perc_reads = 0;
-		$limit_errors_perc_reads = (100 / $nb_values) * 10 if ($nb_values);
+		my ($mean_reads, $limit_errors_reads_min, $limit_errors_reads_max);
 		
 		foreach my $id (sort keys %{$h->{$file}->{values}}) {
 			next if ($id =~ /.+_RC/);
+			
+			my $probably_run_id;
+			my $sample_id = $h->{$file}->{values}->{$id}->{SampleID};
+			if (exists $h->{$file}->{runs_ids}->{by_samples}->{$sample_id}) {
+				my $h_runs_samples;
+				foreach my $run_id (keys %{$h->{$file}->{runs_ids}->{by_samples}->{$sample_id}}) {
+					my $this_nb_samples = $h->{$file}->{runs_ids}->{by_runs}->{$run_id}->{nb_samples};
+					$h_runs_samples->{$this_nb_samples}->{$run_id} = undef;
+				}
+				my @lNb_samples = sort {$a <=> $b} keys %{$h_runs_samples};
+				my $max_samples = $lNb_samples[-1];
+				my @lProb_runs = sort {$a <=> $b} keys %{$h_runs_samples->{$max_samples}};
+				$probably_run_id = $lProb_runs[-1];
+				$mean_reads = $h->{$file}->{runs_ids}->{by_runs}->{$probably_run_id}->{mean_reads};
+			}
+			if (exists $h->{$file}->{values}->{$id}->{'RunID'} && $h->{$file}->{values}->{$id}->{'RunID'} eq 'no_run_info') {
+				$mean_reads = ($total_reads / $nb_samples);
+			}
+			$limit_errors_reads_min = $mean_reads * 0.5;
+			$limit_errors_reads_max = $mean_reads * 1.5;
+			
 			my $tr_id = 'tr_'.$file.'_'.$id;
 			my $html_tr;
 			my $j = 0;
@@ -310,28 +350,75 @@ sub convert_csv_to_json {
 				$index_barcode_sequence = $j if (lc($cat) eq 'index');
 				my $td_id = 'td_'.$file.'_'.$id.'_'.$j;
 				my $value = $h->{$file}->{values}->{$id}->{$j};
-				if ($cat ne '# Reads Norm Only' and $cat =~ /[#%]/ && exists $h->{$file}->{values}->{$id.'_RC'}) {
+				if ($cat ne '# Reads Norm Only' and $cat =~ /[#%]/ && $has_RC) {
 					if ($cat eq '# Reads') {
 						$h->{$file}->{header}[$j] = '# Reads Only';
 					}
 					my $value_RC = $h->{$file}->{values}->{$id.'_RC'}->{$j};
-					if ($cat eq '% Reads' and int($value) >= $limit_errors_perc_reads) {
-						$with_problem++;
-					}
 					$html_tr .= "<td id=\"$td_id\"><center>";
+					#$html_tr .= qq{$total_reads / $nb_samples<br>};
 					$html_tr .= "<table><tr><td><b>Norm</b></td><td style='padding-left:5px;'><center>$value</center></td></tr><tr><td><b>RC</b></td><td style='padding-left:5px;'><center>$value_RC</center></td></tr></table>";
 					$html_tr .= "</center></td>\n";
-					if (lc($cat) eq '# reads') {
+					if ($is_demultiplex_file and lc($cat) eq '# reads') {
 						$with_problem++ if ($value_RC >= $value);
 					}
 				}
 				else {
-					if (lc($cat) eq '% reads') {
-						$with_problem++ if ($value >= $limit_errors_perc_reads);
+					if ($is_demultiplex_file and $has_RC and $cat eq '# Reads Norm Only') {
+						if (int($value) >= $limit_errors_reads_max) {
+							$with_problem++;
+							$h->{$file}->{values}->{$id}->{$j+1} = 'PB +++ reads';
+							$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'PB +++ reads';
+						}
+						elsif (int($value) <= $limit_errors_reads_min) {
+							$with_problem++;
+							$h->{$file}->{values}->{$id}->{$j+1} = 'PB --- reads';
+							$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'PB --- reads';
+						}
+						else {
+							if ($with_problem > 0) {
+								$h->{$file}->{values}->{$id}->{$j+1} = 'PB RC reads';
+								$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'PB RC reads';
+							}
+							else {
+								$h->{$file}->{values}->{$id}->{$j+1} = 'OK';
+								$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'OK';
+							}
+						}
 					}
+					if ($is_demultiplex_file and not $has_RC and $cat eq '# Reads') {
+						if (int($value) >= $limit_errors_reads_max) {
+							$with_problem++;
+							$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'PB +++ reads';
+						}
+						if (int($value) <= $limit_errors_reads_min) {
+							$with_problem++;
+							$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'PB --- reads';
+						}
+					}
+					
+					if ($is_demultiplex_file and $cat eq 'RunID') {
+						if ($probably_run_id) {
+							$value = $probably_run_id;
+						}
+						else {
+							$value = 'mean_reads: '.int($mean_reads);
+						}
+						if (not $has_RC) {
+							if ($with_problem > 0) {
+								$h->{$file}->{values}->{$id}->{$j+1} = $h->{$file}->{values}->{$id}->{'Seems Ok'};
+							}
+							else {
+								$h->{$file}->{values}->{$id}->{$j+1} = 'OK';
+								$h->{$file}->{values}->{$id}->{'Seems Ok'} = 'OK';
+							}
+						}
+					}
+					
+					
 					$html_tr .= "<td id=\"$td_id\">".$value."</td>\n";
 				}
-				if (lc($cat) eq '% of unknown barcodes') {
+				if ($is_top_barcodes_file and lc($cat) eq '% of unknown barcodes') {
 					if ($value >= 15) {
 						if ($h->{$file}->{values}->{$id}->{$index_barcode_sequence} =~ /^A+$/) { $with_ambigous++; }
 						elsif ($h->{$file}->{values}->{$id}->{$index_barcode_sequence} =~ /^T+$/) { $with_ambigous++; }
@@ -340,6 +427,7 @@ sub convert_csv_to_json {
 						else { $with_problem++; }
 					}
 				}
+				
 				$j++;
 				$j = -1 if (not exists $h->{$file}->{header}[$j] or  not exists $h->{$file}->{values}->{$id}->{$j});
 			}
@@ -349,6 +437,8 @@ sub convert_csv_to_json {
 			elsif (lc($id) =~ /undetermined/) { $html_tr = "<tr style='text-align:center;color:red;' id=\"$tr_id\">\n".$html_tr; }
 			else { $html_tr = "<tr style='text-align:center;' id=\"$tr_id\">\n".$html_tr; }
 			$html .= $html_tr;
+			$with_problem = 0;
+			$with_ambigous = 0;
 		}
 		
 		$html .= "</tbody>\n";
