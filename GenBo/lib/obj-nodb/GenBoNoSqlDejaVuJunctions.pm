9f499c7f637dd7ac7698823fc2f77b90216b0bd1
mbras <mbras@sanger.cm.cluster>
2022-09-04 20:41:08 +0200

    MAJ RNA Junctions

---
+++ GenBo/lib/obj-nodb/GenBoNoSqlDejaVuJunctions.pm
@@ -0,0 +1,98 @@
+package GenBoNoSqlDejaVuJunctions;
+# ABSTRACT: An embedded, NoSQL SQLite database with SQL indexing
+use Moose;
+use MooseX::Method::Signatures;
+use strict;
+use warnings;
+use Data::Dumper;
+extends "GenBoNoSqlDejaVu";
+
+
+has extension =>(
+	is		=> 'rw',
+		default => sub {
+		return "dejavu_junctions.lite";
+	}
+);
+
+sub create_table {
+	my ($self,$key1) = @_;
+	return $self->{table}->{$key1}   if (exists $self->{table}->{$key1});
+	my $table_name =$self->name_table_data;
+	$self->{table}->{$key1} = $table_name;
+	return  $self->{table}->{$key1} unless  $self->write();
+	$self->dbh($key1)->do("DROP TABLE IF EXISTS $table_name")  or die $DBI::errstr  if $self->mode eq 'c'  ;
+	$self->dbh($key1)->do("CREATE TABLE if not exists $table_name (_key VARCHAR(250),patients INTEGER, projects INTEGER, _value BLOB,start INTEGER,end INTEGER, variation_type VARCHAR(2), length INTEGER, caller_infos VARCHAR(100))")  or die $DBI::errstr;;
+	return 	$self->{table}->{$key1} ;
+}
+
+sub prepare_junctions_resume {
+	my ($self,$chr) = @_;
+	return $self->{prepare_junctions_resume}->{$chr} if exists $self->{prepare_junctions_resume}->{$chr};
+	my $table_name = $self->create_table($chr);
+	$self->{prepare_junctions_resume}->{$chr} = $self->dbh($chr)->prepare(qq{select $table_name.variation_type,$table_name.start,$table_name.end,$table_name._key,$table_name.patients,$table_name.projects  from $table_name  where end>=? and start<=?  and variation_type=?   });
+	return $self->{prepare_junctions_resume}->{$chr};
+}
+
+sub prepare_junctions {
+	my ($self,$chr) = @_;
+	return $self->{prepare_junctions}->{$chr} if exists $self->{prepare_junctions}->{$chr};
+	my $table_name = $self->create_table($chr);
+	$self->{prepare_junctions}->{$chr} = $self->dbh($chr)->prepare(qq{select $table_name.variation_type,$table_name.start,$table_name.end,$table_name._value from $table_name  where end>=? and start<=?  and variation_type=?   });
+	return $self->{prepare_junctions}->{$chr};
+}
+
+sub get_junction_resume {
+	my ($self,$chr,$start,$end,$type,$dejavu,$seuil) = @_;
+	my $table_name = $self->create_table($chr);
+	my $T2 = $table_name."POSITION";
+	$self->prepare_junctions_resume($chr)->execute($start,$end,$type);
+	my (@lRes);
+	while (my @row = $self->prepare_junctions_resume($chr)->fetchrow_array) { 
+	 	my $start1 = $row[1];
+	 	my $end1 = $row[2];
+	 	my $identity = $self->getIdentityBetweenCNV($start,$end,$start1,$end1);
+	 	next if $identity <  $seuil;
+	 	my $x;
+		$x->{type} = $row[0];
+		$x->{id} = $row[-3];
+		$x->{patients} = $row[-2];
+		$x->{projects} = $row[-1];
+		$x->{same_as} = int($identity).'%';
+		push(@lRes, $x);
+		unless ($dejavu eq "all") {last if scalar(keys %$x) > $dejavu+1};
+	 }
+	return \@lRes;
+}
+
+sub get_junction {
+	my ($self,$chr,$start,$end,$type,$dejavu,$seuil) = @_;
+	my $table_name = $self->create_table($chr);
+	my $T2 = $table_name."POSITION";
+	$self->prepare_junctions($chr)->execute($start,$end,$type);
+	my $x;
+	while (my @row = $self->prepare_junctions($chr)->fetchrow_array) { 
+	 	my $start1 = $row[1];
+	 	my $end1 = $row[2];
+	 	my $identity = $self->getIdentityBetweenCNV($start,$end,$start1,$end1);
+	 	next if $identity <  $seuil;
+		my $z = $self->decode($row[-1]);
+		foreach my $zz (keys %$z){
+			foreach my $zzz (keys %{$z->{$zz}}) {
+				foreach my $zzzz (keys %{$z->{$zz}->{$zzz}}) {
+					$x->{$zz}->{$zzz}->{$zzzz} = $z->{$zz}->{$zzz}->{$zzzz};
+				}
+				$x->{$zz}->{$zzz}->{start} = $start1;
+				$x->{$zz}->{$zzz}->{end} = $end1;
+				$x->{$zz}->{$zzz}->{identity} = $identity;
+			}
+			
+			unless ($dejavu eq "all") {last if scalar(keys %$x) > $dejavu+1};
+		}
+		unless ($dejavu eq "all") {last if scalar(keys %$x) > $dejavu+1};
+	 }
+	 return $x;
+}
+
+
+1;
