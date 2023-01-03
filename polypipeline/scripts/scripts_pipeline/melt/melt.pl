6062d2bce0d40859e5c439b1ca16b8ccc8eedb70
pnitschk <pnitschk@morgan.cm.cluster>
2023-01-03 11:21:20 +0100

    add intspan to melt

--- polypipeline/scripts/scripts_pipeline/melt/melt.pl
+++ polypipeline/scripts/scripts_pipeline/melt/melt.pl
@@ -37,16 +37,46 @@ GetOptions(
 #test
 my $buffer = GBuffer->new();
 my $project = $buffer->newProject( -name => $project_name );
+
+my $bcftools = $buffer->software("bcftools");
+my $bgzip = $buffer->software("bgzip");
+my $tabix = $buffer->software("tabix");
+my $gatk  = $buffer->software("gatk4");
+
 my $patient =  $project->getPatient($patient_name);
 my $dir_in = $project->getAlignmentDir("bwa");
 my $ref = $project->genomeFasta();
 my $bam = $patient->getBamFile();
-my $melt_dir= $project->getCallingPipelineDir("melt-".$patient->name);
+my $melt_dir= $project->getCallingPipelineDir("melt-".$patient->name."-".time);
 my $samtools = $buffer->software("samtools");
+my $dir_out = $melt_dir;
+my $bam_tmp = $dir_out."/".$patient->name.time.".bam";
+my $bed = $dir_out."/".$patient->name.time.".bed";
+
+#my $list = $dir_out."/".$patient->name.".list";
+
+unlink $bed if -e $bed;
+open(BED,">$bed");
+#open(LIST,">$list");
+foreach my $chr (@{$project->getChromosomes}){
+	my $chr_name = $chr->fasta_name;
+	#next unless $chr_name eq "18";
+#	print LIST $chr->name."\t".$chr->fasta_name."\n";
+	#getIntSpanCapture
+	#getIntSpanCaptureForCalling
+	my @line = intspanToBed($chr_name,$chr->getIntSpanCapture(50) );
+	print BED join("\n",@line)."\n" if @line;
+}
+
+close(BED);
+#system("sambamba slice $bam -L $bed -o $bam_tmp;samtools index $bam_tmp ");
+#warn $bam_tmp;
+#die();
+
 my $meltd = "/software/distrib/MELT/MELTv2.2.2";
 my $melt = "java -jar $meltd/MELT.jar Single -a -c 8 ";
 #java -jar MELT.jar Single -a -c 8 -h /data-isilon/public-data/genome/HG19/fasta/all.fa -bamfile /data-isilon/sequencing/ngs/NGS2018_2224/HG19/align/bwa/1806245.bam -n ./add_bed_files/1KGP_Hg19/hg19.genes.bed  -w /data-isilon/sequencing/ngs/NGS2018_2224/HG19/align/test -t ./me_refs/list.txt
-	my $dir_out = $melt_dir;
+
 	system("mkdir $dir_out && chmpd a+rwx $dir_out ") unless -e $dir_out;
 	#system("sambamba slice $bam ".$chr->fasta_name." >$bout && samtools index $bout");
 	system("$melt -h $ref -bamfile $bam -n $meltd/add_bed_files/1KGP_Hg19/hg19.genes.bed  -w $dir_out -t $meltd/me_refs/list.txt  -exome 1");
@@ -64,9 +94,9 @@ my $melt = "java -jar $meltd/MELT.jar Single -a -c 8 ";
 	unless (keys %$files){
 		my $fileout = $project->getVariationsDir("melt")."/".$patient->name.".vcf";
 		print_empty_vcf($fileout,$patient);
-		system("bgzip $fileout");
+		system("$bgzip -f $fileout");
 		$fileout .= ".gz";
-		system("tabix -p vcf $fileout");
+		system("$tabix -f -p vcf $fileout");
 		exit(0);
 		#write empty vcf;
 	}
@@ -74,45 +104,22 @@ my $melt = "java -jar $meltd/MELT.jar Single -a -c 8 ";
 	my $tvcf2 = $dir_out."/".$patient->name.".".time.".2.vcf.gz";
 	my $list_file = join(" ",values %$files);
 	
+
+#close (LIST);
+
+
+
+system("$bgzip $bed ; $tabix $bed.gz");
+$bed = $bed.".gz";
+	
+	
  	my $fileout = $project->getVariationsDir("melt")."/".$patient->name.".vcf.gz";
-	warn "bcftools concat $list_file | bcftools view  - -U -c 1  > $tvcf; gatk UpdateVCFSequenceDictionary -V $tvcf --source-dictionary /data-isilon/public-data/genome/HG19/fasta/all.dict  --output $tvcf2 --replace; bcftools sort $tvcf2";
-	system ("bcftools concat $list_file | bcftools view  - -U -c 1  > $tvcf; gatk UpdateVCFSequenceDictionary -V $tvcf --source-dictionary /data-isilon/public-data/genome/HG19/fasta/all.dict  --output $tvcf2 --replace; bcftools sort $tvcf2 -O z -o $fileout; tabix -f -p vcf $fileout");
+	#warn "$bcftools concat $list_file | $bcftools view - -T $bed | $bcftools view  - -U -c 1  > $tvcf; $gatk UpdateVCFSequenceDictionary -V $tvcf --source-dictionary /data-isilon/public-data/genome/HG19/fasta/all.dict  --output $tvcf2 --replace; $bcftools sort $tvcf2 -O z -o $fileout; tabix -f -p vcf $fileout";
+	my $cmd = qq{$bcftools concat $list_file | $bcftools view - -T $bed | perl -lane 's/GL,Number=\\d/GL,Number=G/;print \$_' | $bcftools view  - -U -c 1  > $tvcf; $gatk UpdateVCFSequenceDictionary -V $tvcf --source-dictionary /data-isilon/public-data/genome/HG19/fasta/all.dict  --output $tvcf2 --replace; $bcftools sort $tvcf2 -O z -o $fileout; $tabix -f -p vcf $fileout};
+	warn $cmd;
+	system ($cmd);
 	exit(0);
-	my $f2 = "/data-isilon/bipd-src/pnitschk/git/repository/polypipeline/scripts/scripts_pipeline/melt/1806245.vcf.gz";
-	my $v = Bio::DB::HTS::VCF->new( filename => $f2 );
-	my $header = $v->header();
-	warn $header->get_sample_names()->[0];
-	#die if $header->num_samples() ne 1;
 	
-	my %hashRes;
-	my $iter = $v->query("chr10:1-89685296");
-	
-		while (my $row = $iter->next) {
-			my $structType = "mei";
-			my $alleles = $row->get_alleles();
-			my $allele_index = 1;
-			warn $row->reference;
-			
-			#warn Dumper $row->get_format($header, "DP") ;
-			
-			#warn Dumper $row->get_format_type($header, "GT") ;
-			#warn Dumper $header;
-			warn Dumper $row->get_format($header, "GT") ;
-			warn Dumper $row->get_format($header, "AD") ;
-			warn Dumper $row->get_genotypes($header);
-			
-			next ;
-			die();
-			#warn Dumper $row->get_format($header, "AD") ;
-			next ;
-			for my $a (@$alleles) {
-				
-				warn Dumper $row;
-				
-  				printf( "(%s, %s)\n", $a, $row->get_variant_type($allele_index++) ) ;
-			}
-		}
-	die();
 	
 	sub print_empty_vcf {
 	my ( $file, $patient ) = @_;
@@ -123,5 +130,25 @@ my $melt = "java -jar $meltd/MELT.jar Single -a -c 8 ";
 	  . $patient->name . "\n";
 	close OUT;
 	
+}
+
+
+
+
+sub intspanToBed{
+	my ($chr_name,$intspan) = @_;
+	my $iter = $intspan->iterate_runs();
+	my @tt;
+	my $size = 0;
+    while (my ( $from, $to ) = $iter->()) {
+    	#warn "+".$from."-".$to if ($from<=31263320);
+    	warn $from."-".$to if ($from<=31263320 && $to >= 31263320);
+    	$size += abs($from-$to);
+    		push(@tt,$chr_name."\t".$from."\t".$to);
+    	
+    }
+    warn $chr_name." ".$size;
+	#	my @tt = map{$_ = $chr->ucsc_name."\t".$_} split(";",$intspan->as_string({ sep => ";", range => "\t" }) );
+		return @tt;
 }
 	
