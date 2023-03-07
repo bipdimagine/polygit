0c239ea0466058f69d0d4ade5c7ac99ed3cdceeb
pnitschk <pnitschk@morgan.cm.cluster>
2023-03-07 20:21:25 +0100

    chrom

--- GenBo/lib/obj-nodb/GenBoChromosome.pm
+++ GenBo/lib/obj-nodb/GenBoChromosome.pm
@@ -1424,13 +1424,13 @@ sub get_lmdb_variations {
 	
 	my $dir_out_rocks = $self->project->rocks_cache_dir;
 	warn $dir_out_rocks;
-	if ($mode eq "c"){
+	if ($mode eq "c") {
 		if ($rocks) {
 			system ("mkdir $dir_out && chmod a+rwx $dir_out" ) unless -e  $dir_out_rocks;
 			$self->{lmdb}->{$hindex} =  $self->get_rocks_variations($mode);
 		}
 		else {
-			$self->{lmdb}->{$hindex} =  $self->get_rocks_variations($mode,$dir_out);
+			$self->{lmdb}->{$hindex} =  $self->get_old_lmdb_variations($mode,$dir_out);
 		}
 	}
 	else {
