9244b4035f038e74f0b2b4a93a1253bd65b94265
masson <masson@morgan.cm.cluster>
2023-08-02 12:27:30 +0200

    change rmdup rna

--- polypipeline/dragen/scripts/dragen_command.pl
+++ polypipeline/dragen/scripts/dragen_command.pl
@@ -143,7 +143,7 @@ if ($version && exists $pipeline->{align} ){
 else {
 my ($fastq1,$fastq2) = dragen_util::get_fastq_file($patient,$dir_pipeline);
 
- $param_align = " -1 $fastq1 -2 $fastq2 --RGID $runid  --RGSM $prefix --enable-map-align-output true --enable-rna=true ";
+ $param_align = " -1 $fastq1 -2 $fastq2 --RGID $runid  --RGSM $prefix --enable-map-align-output true --enable-duplicate-marking true --enable-rna=true ";
 }
 
 if (exists $pipeline->{count}){
