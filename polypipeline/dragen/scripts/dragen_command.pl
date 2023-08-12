3c6e01c127237d22ca9dcce75fa45051c59582f2
pnitschk <pnitschk@morgan.cm.cluster>
2023-08-12 23:53:56 +0200

    debug move and delete bam

--- polypipeline/dragen/scripts/dragen_command.pl
+++ polypipeline/dragen/scripts/dragen_command.pl
@@ -132,6 +132,7 @@ my $tmp = "/staging/tmp";
 my $cmd_dragen = qq{dragen -f -r $ref_dragen --output-directory $dir_pipeline --intermediate-results-dir $tmp --output-file-prefix $prefix };
 my $runid = $patient->getRun()->id;
 my $bam   = $dir_pipeline."/".$patient->name.".bam";
+my ($fastq1,$fastq2);
 my $align = "true";
 if (-e $bam){
 	$align = "false";
@@ -153,7 +154,7 @@ if ($version && exists $pipeline->{align} ){
 
 else {
 	
-my ($fastq1,$fastq2) = dragen_util::get_fastq_file($patient,$dir_pipeline);
+ ($fastq1,$fastq2) = dragen_util::get_fastq_file($patient,$dir_pipeline);
 
  $param_align = " -1 $fastq1 -2 $fastq2 --RGID $runid  --RGSM $prefix --enable-map-align-output true --enable-duplicate-marking true --enable-rna=true ";
 }
@@ -168,7 +169,10 @@ my ($fastq1,$fastq2) = dragen_util::get_fastq_file($patient,$dir_pipeline);
 $cmd_dragen .= $param_umi." ".$param_align;
 $patient->update_software_version("dragen",$cmd_dragen);
 my $exit = system(qq{$Bin/../run_dragen.pl -cmd=\"$cmd_dragen\"}) ;#unless -e $f1;
+unlink $fastq1 if  $fastq1 =~ /pipeline/;
+unlink $fastq2 if $fastq2 =~ /pipeline/;;
 die if $exit != 0;
+
 }
 
 
