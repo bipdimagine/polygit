2e4a07434616dff72b859d9112798c9288e90501
mbras <mbras@sanger.cm.cluster>
2023-09-29 15:12:43 +0200

    update index-rna

--- GenBo/lib/obj-nodb/GBuffer.pm
+++ GenBo/lib/obj-nodb/GBuffer.pm
@@ -1080,7 +1080,6 @@ foreach my $database (keys %{$self->{lmdb_score}}){
 }
 delete 	$self->{lmdb_score};
 delete $self->{lmdb};
-warn "end buffer "." ".time;
 }
 
 sub intersection {
--- polymorphism-cgi/json_output_nodb/projects_json_rna_analysis.pl
+++ polymorphism-cgi/json_output_nodb/projects_json_rna_analysis.pl
@@ -54,7 +54,23 @@ my $viewer = $cgi->param('viewer');
 my $defidiag = $cgi->param('defidiag');
 getProjectListsRNA($buffer,$login,$pwd,$project_query) if $action eq "list";
 
+sub get_path_rna_seq_junctions_root {
+	my ($h_proj) = @_;
+	my $path = $buffer->getDataDirectory("root")."/ngs/".$h_proj->{name}."/".$h_proj->{relname}."/analysis/";
+	return $path;
+}
 
+sub get_path_rna_seq_polyrna_root {
+	my ($h_proj) = @_;
+	my $path = $buffer->getDataDirectory("root")."/ngs/".$h_proj->{name}."/".$h_proj->{relname}."/polyRNA/";
+	return $path if -d $path;
+	my @lPotentialRelease = ('HG19', 'HG19_CNG', 'HG19_MT', 'HG38', 'HG38-ERCC', 'MM38', 'MM39');
+	foreach my $rel2 (@lPotentialRelease) {
+		my $alt_path = $buffer->getDataDirectory("root")."/ngs/".$h_proj->{name}."/".$rel2."/polyRNA/";
+		return $alt_path if -d $alt_path;
+	}
+	return $path;
+}
  
 sub getProjectListsRNA {
 	my ( $buffer, $login, $pwd, $project_name ) = @_;
@@ -64,6 +80,10 @@ sub getProjectListsRNA {
 	my $is_BIPD_login = $buffer->getQuery->isLoginSTAFF($login);
 	my $list_proj = $buffer->getQuery->getListProjectsRnaSeqFromLoginPwd($login, $pwd, $project_name);
 	my ($hDone, $h_ok);
+	
+	
+	
+	
 	foreach my $h (@$list_proj) {
 		print '.';
 		my $h_users;
@@ -73,27 +93,26 @@ sub getProjectListsRNA {
 		}
 		my $name = $h->{name};
 		next if exists $hDone->{name};
-		my $b1 = GBuffer->new;
-		my $p1 = $b1->newProject( -name => $name );
-		$h->{button} = '';
-#		if (-d $p1->get_path_rna_seq_junctions_root()) {
-#			my $project_name = $h->{name};
-#			#my $path_function_ok = $p1->get_path_rna_seq_junctions_root().'/AllRes/';
-#			#$project_name = 'disabled' if (not -d $path_function_ok);
-#			$h->{button} = '1::'.$project_name;
-#		}
-#		$h->{button} = '2::'.$h->{name} if (-d $p1->get_path_rna_seq_polyrna_root());
-		my $path = $p1->get_path_rna_seq_junctions_analyse_all_res();
-		my @lbuttons;
+		my $path = get_path_rna_seq_junctions_root($h);
 		
+		$h->{button} = '';
+		my @lbuttons;
+		my @lPatNames = split(';', $h->{patient_name});
 		if (-d $path) {
 			my $ok;
-			foreach my $patient (@{$p1->getPatients()}) { 
-				my $dragen_file = $p1->getVariationsDir('dragen-sj').'/'.$patient->name().'.SJ.out.tab.gz';
-				$ok = 1 if (-e $dragen_file);
+			foreach my $patient_name (@lPatNames) { 
+				my $dragen_file = $path.'/../variations/dragen-sj/'.$patient_name.'.SJ.out.tab.gz';
+				if (-e $dragen_file) {
+					$ok = 1;
+					last;
+				}
 			}
-			my $se_file = $path.'/allResSE.txt' if (-e $path.'/allResSE.txt');
-			my $ri_file = $path.'/allResRI.txt' if (-e $path.'/allResRI.txt');
+			my $se_file = $path.'/allResSE.txt';
+			my $ri_file = $path.'/allResRI.txt';
+			$ok = 1 if (-e $se_file);
+			$ok = 1 if (-e $ri_file);
+			$se_file = $path.'/AllRes/allResSE.txt';
+			$ri_file = $path.'/AllRes/allResRI.txt';
 			$ok = 1 if (-e $se_file);
 			$ok = 1 if (-e $ri_file);
 			push(@lbuttons, '1::'.$h->{name}) if ($ok);
@@ -101,15 +120,19 @@ sub getProjectListsRNA {
 		}
 		else {
 			my $ok_dragen;
-			foreach my $patient (@{$p1->getPatients()}) { 
-				my $dragen_file = $p1->getVariationsDir('dragen-sj').'/'.$patient->name().'.SJ.out.tab.gz';
-				$ok_dragen = 1 if (-e $dragen_file);
+			foreach my $patient_name (@lPatNames) { 
+				my $dragen_file = $path.'/../variations/dragen-sj/'.$patient_name.'.SJ.out.tab.gz';
+				if (-e $dragen_file) {
+					$ok_dragen = 1;
+					last;
+				}
 			}
 			push(@lbuttons, '1::'.$h->{name}) if ($ok_dragen);
 			$hDone->{$name} = undef if $ok_dragen;
 		}
 		
-		if (-d $p1->get_path_rna_seq_polyrna_root()) {
+		my $path_polyRNA = get_path_rna_seq_polyrna_root($h);
+		if (-d $path_polyRNA) {
 			push(@lbuttons, '2::'.$h->{name});			
 			$hDone->{$name} = undef;
 		}
