047e3bc65e5856ef2ed9d8493b3eba40a24230cb
mbras <mbras@sanger.cm.cluster>
2024-02-12 10:19:49 +0100

    add RNASeqSea in bds_pipeline

--- polypipeline/bds_pipeline.pl
+++ polypipeline/bds_pipeline.pl
@@ -269,6 +269,7 @@ my $steps = {
 				"muc1" => sub {$pipeline->muc1(@_)},
 				"advntr" => sub {$pipeline->advntr(@_)},
 				"star_align" => sub {$pipeline->star_align(@_)},
+				"rnaseqsea_capture" => sub {$pipeline->rnaseqsea_capture(@_)},
 			};
 			
 my @types_steps = ('pipeline','calling');
