1c422f3623d40af8d9b294ed867452b55f5babf0
mbras <mbras@sanger.cm.cluster>
2024-03-21 14:43:36 +0100

    polyrnaseq - new link

--- GenBo/lib/obj-nodb/GBuffer.pm
+++ GenBo/lib/obj-nodb/GBuffer.pm
@@ -1652,11 +1652,19 @@ sub log2 {
 #}
 
 
-
+sub get_url_polyrna {
+	my ($self) = shift;
+	return $self->get_base_url_polyrna().':'.$self->get_port_url_polyrna();
+}
 
 sub get_base_url_polyrna {
 	my ($self) = shift;
 	return $self->config->{polyrna}->{base_url_polyrna};
+}
+
+sub get_port_url_polyrna {
+	my ($self) = shift;
+	return $self->config->{polyrna}->{port_url_polyrna};
 }
 
 sub get_polyrna_file_server_to_docker {
