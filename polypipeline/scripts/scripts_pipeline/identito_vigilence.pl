b1b877ca36e35348f93fdfdfc7be6338449d64d8
pnitschk <pnitschk@morgan.cm.cluster>
2024-09-30 09:54:30 +0200

    add 4 for low deph and new script

--- polypipeline/scripts/scripts_pipeline/identito_vigilence.pl
+++ polypipeline/scripts/scripts_pipeline/identito_vigilence.pl
@@ -62,7 +62,6 @@ foreach my $var_id (@list) {
 	#next;
 	unless (defined $v->vector_id){
 		foreach my $patient (@{$project->getPatients()}) {
-			warn $v->id;
 			push(@{$id_bc->{$patient->name}->{code}},1);
 		}
 		next;
@@ -77,7 +76,12 @@ foreach my $var_id (@list) {
 		}
 		else {
 			my $depth= $patient->depth($v->getChromosome()->id(),$v->start,$v->start+1);
-			push(@{$id_bc->{$patient->name}->{code}},1);
+			if ($depth > 10){
+				push(@{$id_bc->{$patient->name}->{code}},1) ;
+			}
+			else {
+					push(@{$id_bc->{$patient->name}->{code}},4) ;
+			}
 		#	warn $depth->[0];
 			
 		}
