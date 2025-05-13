0e4a1421f084de56dbee60aad05cce11b7841a78
pnitschke <patrick.nitschke@parisdescartes.fr>
2025-05-13 10:08:45 +0200

    4

--- polypipeline/scripts/scripts_pipeline/melt/melt.pl
+++ polypipeline/scripts/scripts_pipeline/melt/melt.pl
@@ -63,17 +63,34 @@ my @header = `$samtools view -H  $bam_prod`;
 chomp(@header);
 my ($umi) = grep{$_ =~ /umi-correction-scheme/} @header;
 if ($umi){
-		$bam_tmp = $dir_out."/".$patient->name.".umi.bam";
+	
 		my $cmd = qq{samtools view -h $bam_prod }.q{| perl -pe 'if (!/^@/) { @fields = split("\t"); die() if length($fields[9]) ne length($fields[10]); $fields[10] = "I" x length($fields[9]); $_ = join("\t", @fields); }' | samtools view -Sb - >}.$bam_tmp.q{ && samtools index }.$bam_tmp.' -@ 5';
 		warn $cmd;
 		system($cmd);
 		
 	die() unless -e $bam_tmp.".bai";
 }
+elsif ($bam_prod =~/.cram/){
+		my $list;
+		foreach my $chr (@{$project->getChromosomes}){
+			push(@$list,$chr->fasta_name);
+		} 
+	
+		$bam_tmp = $dir_out."/".$patient->name.".cram2bam.bam";
+		my $samtools = $buffer->software("samtools");
+		warn "$samtools view -@ $fork  -T $ref  $bam_prod ".join(" ",@$list) ." -o $bam_tmp --write-index";
+		system("$samtools view -@ $fork  -T $ref  $bam_prod ".join(" ",@$list) ." -o $bam_tmp --write-index");
+}
 else {
 system("ln -s $bam_prod $bam_tmp");
 system("ln -s $bam_prod.bai $bam_tmp.bai");
 }
+
+my $bam_tmp2 = $bam_tmp;
+if ($bam_prod =~/.cram/){
+	
+}
+$dir_out."/".$patient->name.".cram.bam";
 my $bed = $dir_out."/".$patient->name.time.".bed";
 
 #my $list = $dir_out."/".$patient->name.".list";
@@ -126,6 +143,7 @@ close LIST;
 		my $gtf ="";
 		#system qq{cat gencode.v43.annotation.gtf |  awk 'OFS="\t" {if ($3=="gene") {print \$1,\$4-1,\$5,\$10,\$16,\$7}}' | tr -d '";'" > $bed};
 		warn "$melt -h $ref -bamfile $bam_tmp  -w $dir_out -t $list  -n /data-isilon/software/distrib/centos/MELTv2.2.2/add_bed_files/1KGP_Hg19/hg19.genes.bed -b hs37d5/NC_007605 && touch $done" ;
+		system("$melt -h $ref -bamfile $bam_tmp  -w $dir_out -t $list  -n /data-isilon/software/distrib/centos/MELTv2.2.2/add_bed_files/1KGP_Hg19/hg19.genes.bed -b hs37d5/NC_007605 && touch $done");
 		die($done);	
 		#system("$melt -h $ref -bamfile $bam_tmp  -w $dir_out -t $list  -n /data-isilon/software/distrib/centos/MELTv2.2.2/add_bed_files/1KGP_Hg19/hg19.genes.bed -b hs37d5/NC_007605 && touch $done") unless -e $done;
 		
