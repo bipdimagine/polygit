05da513352bf3e2009f3a40574c7ef4467303cc8
mperin <mperin@sanger.cm.cluster>
2025-09-19 17:06:26 +0200

    canvas.pl correction final_temp_vcf

--- polypipeline/scripts/scripts_pipeline/canvas.pl
+++ polypipeline/scripts/scripts_pipeline/canvas.pl
@@ -18,13 +18,13 @@ use List::Util qw(sum);
 
 
 
- my $project_name;
- my $fork;
- my $callable_intspan_file;
- my $patient_name;
- #my $low_calling;
- my $method;
- my $version;
+my $project_name;
+my $fork;
+my $callable_intspan_file;
+my $patient_name;
+#my $low_calling;
+my $method;
+my $version;
 GetOptions(
 	'project=s'   => \$project_name,
 	"fork=s"  => \$fork,
@@ -33,7 +33,7 @@ GetOptions(
 );
 
 
- my $methods = {
+my $methods = {
 			"manta"				=> { "method" => sub{calling_SV::manta(@_)}, priority =>1}, 
 };
 
@@ -45,17 +45,17 @@ my $samtools  = $project->getSoftware('samtools');
 my $tabix  = $project->getSoftware('tabix');
 my $ref =  $project->genomeFasta();
 my $patient = $project->getPatient($patient_name);
- my $bam = $patient->getBamFile() ;
-  my $dirout= $project->getCallingPipelineDir("canvas-".$patient->name);
- if ($bam =~ /\.cram/){
+my $bam = $patient->getBamFile() ;
+my $dirout= $project->getCallingPipelineDir("canvas-".$patient->name);
+if ($bam =~ /\.cram/){
  	#samtools view -b -T ref.fa -o output_bam.bam input_cram.cram
  	my $cram = $bam;
  	$bam = $dirout."/".$patient->name.".bam";
  	warn "$samtools view -b -T $ref -o $bam $cram --thread $fork && $samtools index $bam --threads $fork";
  	system("$samtools view -b -T $ref -o $bam $cram -\@ $fork && $samtools index $bam -\@ $fork");
  	die() unless -e $bam.".bai";
- }
- 
+}
+
 my $dd .="$dirout/".$patient->name().".".time;
 system("mkdir -p $dirout");
 warn $dd;
@@ -68,13 +68,13 @@ my $genome = $project->genomeFasta();
 #-r /data-xfs/public-data/HG19/canvas/genome.fa -g /data-xfs/public-data/HG19/canvas --filter-bed=/data-xfs/public-data/HG19/canvas/filter13.bed -n toto  -o toto --population-b-allele-vcf=/data-xfs/public-data/HG19/canvas/dbsnp.vcf --ploidy-vcf=./ploidy.vcf
 my $ploidy = "$dir_canvas/ploidy_sample_female.vcf";
 $ploidy = "$dir_canvas/ploidy_sample_male.vcf" if $patient->isMale();
- my $cmd = " $canvas Germline-WGS --bam=$bam -r $genome -g $dir_canvas --filter-bed=$dir_canvas/filter13.bed -n $patient_name -o $dd --ploidy-vcf=$ploidy --population-b-allele-vcf=$dir_canvas//dbsnp.vcf";
+ my $cmd = " $canvas Germline-WGS --bam=$bam -r $genome -g $dir_canvas --filter-bed=$dir_canvas/filter13.bed -n $patient_name -o $dd --ploidy-vcf=$ploidy --population-b-allele-vcf=$dir_canvas/dbsnp.vcf";
  warn $cmd;
  system($cmd);
- my $final_temp_vcf = "$dirout/$patient_name/CNV.vcf.gz";
+ my $final_temp_vcf = "$dd/CNV.vcf.gz";
  die($final_temp_vcf) unless -e $final_temp_vcf;
  
-  my $final_dir =  $project->getVariationsDir("canvas") ;
+ my $final_dir =  $project->getVariationsDir("canvas") ;
  my $cmd2 = "mv  $final_temp_vcf $final_dir/$patient_name.vcf.gz && $tabix -p vcf $final_dir/$patient_name.vcf.gz";
  warn $cmd2;
  system("$cmd2");
