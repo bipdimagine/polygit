6bf941fe753811e7bc314e058adece75b2ff4c31
pnitschke <patrick.nitschke@parisdescartes.fr>
2025-10-06 10:23:55 +0200

    hotspot in pipeline

---
+++ polypipeline/scripts/scripts_pipeline/hotspot/hotspot.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/perl
+use FindBin qw($Bin);
+use strict;
+use FindBin qw($Bin);
+use lib "$Bin/../../../../GenBo/lib/obj-nodb/";
+use lib "$Bin/../../../packages/";
+use  File::Temp;
+use Data::Dumper;
+use Getopt::Long;
+use Carp;
+use GBuffer;
+
+use Storable qw(store retrieve freeze);
+ use List::MoreUtils qw(natatime);
+ 
+ my $project_name;
+my $final_vcf;
+my $log_file;
+my $patient_name;
+my $fork;
+
+GetOptions(
+	'project=s'   => \$project_name,
+	"patient=s"=>\$patient_name,
+);
+my $date = `date`;
+chomp($date);
+
+my $buffer = GBuffer->new();
+my $project = $buffer->newProject( -name => $project_name );
+my $rocks = $project->hotspot_rocks("c");
+foreach my $patient (@{$project->getPatients()}){
+my $hotspot = $patient->hotspot();
+$rocks->put($patient->id,$hotspot);
+}
+ $project->hotspot_rocks("close");
+exit(0);
