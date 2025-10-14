ad6011e5863e9f375b8ad05f800db374207e344a
pnitschke <patrick.nitschke@parisdescartes.fr>
2025-10-15 00:22:46 +0200

    change methods tiny dir

--- polypipeline/packages/bds_cache_rocks.pm
+++ polypipeline/packages/bds_cache_rocks.pm
@@ -52,7 +52,7 @@ sub tiny_rocks {
 	my $stepname = $projectName."@".$type;
 #	my $fileout = $self->project->project_log()."/dejavu_parquet.log";
 	# my $dir_parquet = $self->project->rocks_directory_beegfs()."/tiny_rocks/".$self->project->name.".store";;
-	my $fileout = $self->project->rocks_directory_beegfs()."/tiny_rocks/".$self->project->name.".store";
+	my $fileout = $self->project->tiny_rocks_cache_dir()."/".$self->project->name.".store";
 	warn $fileout;
 	my $cmd = "/usr/bin/perl $Bin/../polymorphism-cgi/cache_nodb/scripts/rocks/tiny_rocks.pl  -project=$projectName ";
 	my $job_bds = job_bds->new(cmd=>[$cmd],name=>$stepname,ppn=>$ppn,filein=>[$filein],fileout=>$fileout,type=>$type,dir_bds=>$self->dir_bds);
