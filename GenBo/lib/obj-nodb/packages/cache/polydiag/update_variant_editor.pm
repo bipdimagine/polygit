33db736d88d5bdb4dea1313c15019bb3c3d80bf4
pnitschke <patrick.nitschke@parisdescartes.fr>
2025-10-18 23:04:05 +0200

    1

--- GenBo/lib/obj-nodb/packages/cache/polydiag/update_variant_editor.pm
+++ GenBo/lib/obj-nodb/packages/cache/polydiag/update_variant_editor.pm
@@ -3060,6 +3060,7 @@ sub get_hash_genes_dude {
  	 		$pm->finish(0,$hGenes_dude_tmp);
  	 	}
 		$pm->wait_all_children();
+		warn " -----> ".abs(time -$ti);
 		$patient->getProject->buffer->dbh_reconnect();
 	}
 	$no->close();
