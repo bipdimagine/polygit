ea9b3acd5ef09f3ab9d8a2b06d9a22e63c9a05b1
mbras <mbras@sanger.cm.cluster>
2025-10-30 16:03:35 +0100

    archive in includes

--- polyweb/js/polyjs/include_OLD.js
+++ polyweb/js/polyjs/include_OLD.js
@@ -67,6 +67,10 @@ else if (href.indexOf("mbras", 1) > -1) {
 	url_path = "/cgi-bin/mbras/polymorphism-cgi/";
 }
 
+else if (href.indexOf("archive", 1) > -1) {
+	url_path = "/cgi-bin/archive/polymorphism-cgi/";
+}
+
 else if (href.indexOf("eollivie", 1) > -1) {
 	url_path = "/cgi-bin/eollivie/polymorphism-cgi/";
 }
