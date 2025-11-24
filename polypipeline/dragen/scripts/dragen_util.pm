6554716924d8bdda66fa3704ebe9f710a5bd8000
pnitschke <patrick.nitschke@parisdescartes.fr>
2025-11-24 16:35:16 +0100

    add I1 and I2 in cat fastq

--- polypipeline/dragen/scripts/dragen_util.pm
+++ polypipeline/dragen/scripts/dragen_util.pm
@@ -7,12 +7,17 @@ sub get_fastq_file {
 	
 	my $name=$patient->name();
 	$dir_fastq = $patient->getSequencesDirectory() unless $dir_fastq;
+	warn $patient->name;
 	my $files_pe1 = file_util::find_file_pe($patient,"",$dir_fastq,1);
 	return unless @$files_pe1;
 	my $cmd;
 	my @r1;
 	my @r2;
+	my $find_I1;
 	foreach my $cp (@$files_pe1) {
+		$find_I1 ++ if $cp->{R1} =~ /_I1_0/;;
+		next if $cp->{R1} =~ /_I1_0/;
+		die() if $cp->{R2} !~ /_R2_/;
 		my $file1 = $dir_fastq."/".$cp->{R1};
 		die($file1) unless -e  $file1;
 		my $file2 = $dir_fastq."/".$cp->{R2};
@@ -27,7 +32,33 @@ sub get_fastq_file {
 	#if ($step eq "align"){
 		system "cat $cmd1 > $fastq1";# unless -e $fastq1;
 		system "cat $cmd2 > $fastq2";# unless -e $fastq2;
+	
+	if ($find_I1){
+		
+			my @r1;
+	my @r2;
+		foreach my $cp (@$files_pe1) {
+		next if $cp->{R1} =~ /_R1_/;
+		die() if $cp->{R2} !~ /_I2_/;
+		my $file1 = $dir_fastq."/".$cp->{R1};
+		die($file1) unless -e  $file1;
+		my $file2 = $dir_fastq."/".$cp->{R2};
+		die($file2) unless -e  $file1;
+		push(@r1,$file1);
+		push(@r2,$file2);##
+	}
+	my $cmd1 = join(" ",@r1);
+	my $cmd2 = join(" ",@r2);
+	my $fastq1 = $dir_pipeline."/".$patient->name."_S1_L001_I1_001.fastq.gz";
+	my $fastq2 = $dir_pipeline."/".$patient->name."_S1_L001_I2_001.fastq.gz";
+	#if ($step eq "align"){
+		system "cat $cmd1 > $fastq1";# unless -e $fastq1;
+		system "cat $cmd2 > $fastq2";# unless -e $fastq2;
+		
+	}	
+		
 	#}
+	die();
 	return  ($fastq1,$fastq2);
 }
 
