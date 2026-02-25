ea60aa6a9ef3dd0bd6d3843fc6e30c76d53b14df
pnitschke <patrick.nitschke@parisdescartes.fr>
2026-02-25 16:23:43 +0100

    scritp aphp

--- polypipeline/scripts/scripts_pipeline/deepvariant/create_run_deepvariant.pl
+++ polypipeline/scripts/scripts_pipeline/deepvariant/create_run_deepvariant.pl
@@ -19,22 +19,34 @@ use Set::IntSpan::Fast::XS;
  use IPC::Open2;
  use List::MoreUtils qw(natatime);
  
+my $RED    = "\033[31m";
+my $GREEN  = "\033[32m";
+my $YELLOW = "\033[33m";
+my $BLUE   = "\033[34m";
+my $BOLD   = "\033[1m";
+my $RESET  = "\033[0m";
+ 
  my $project_name;
 my $final_vcf;
 my $log_file;
 my $fork;
 my $force;
-
+my $type;
 GetOptions(
 	'project=s'   => \$project_name,
+	'type=s'  => \$type,
 	"log=s" =>\$log_file,
 	"vcf=s" => \$final_vcf,
 	"fork=s" =>\$fork,
 	"force=s" =>\$force,
 );
+foreach my $t (split(",",$type)){
+die("please use -type=deepvariant or -type=deepsomatic or -type=deepvariant,deepsomatic") if $type ne "deepvariant" or $type ne "deepsomatic";
 my $date = `date`;
 chomp($date);
-
+print "\n" . "=" x 60 . "\n";
+	print "----  RUN  $RED $type $RESET --  \n ";
+print "\n" . "=" x 60 . "\n";	
 my $buffer = GBuffer->new();
 my $project = $buffer->newProject( -name => $project_name );
 
@@ -54,9 +66,15 @@ open (BED , ">".$bed);
 	print BED join("\n",@zbed);
 close(BED);
 }
+
+foreach my $t (split(",",$type)){
 my $ref               = $project->genomeFasta();
 my $deepvariant = $buffer->software("deepvariant-sif");
+if ($type =~somatic){
+	$deepvariant = $buffer->software("deepsomatic-sif");
+}
 my $singularity = $buffer->software("singularity-run");
+
 my $bcftools =  $buffer->software("bcftools-sif");
 foreach my $patient (@{$project->getPatients}){
 my $bam                 = $patient->getBamFile();
@@ -66,19 +84,22 @@ system("mkdir $dir_gvcf_out && chmod a+rwx $dir_gvcf_out") unless -e $dir_gvcf_o
 my $vcf_out = $dir_gvcf_out."/".$patient->name.".deep.vcf.gz";
 my $dir_gvcf_tmp = "/tmp/";
 my $cmd;
+
 $fork =20 if $fork >20;
 
-
+my $nsh = "";
+$nsh = "-num_shards=$fork" if $fork;
 if ($project->isGenome) {
- $cmd = qq{$singularity $deepvariant run_deepvariant  --model_type=WGS --intermediate_results_dir=$dir_gvcf_tmp --ref=$ref --reads=$bam --output_vcf=$vcf_out -num_shards=$fork};
+ $cmd = qq{$singularity $deepvariant run_deepvariant  --model_type=WGS --intermediate_results_dir=$dir_gvcf_tmp --ref=$ref --reads=$bam --output_vcf=$vcf_out $nsh};
 }
 else {
- $cmd = qq{$singularity $deepvariant run_deepvariant   --model_type=WES --ref=$ref --reads=$bam --regions=$bed --output_vcf=$vcf_out -num_shards=$fork};
+ $cmd = qq{$singularity $deepvariant run_deepvariant   --model_type=WES --ref=$ref --reads=$bam --regions=$bed --output_vcf=$vcf_out $nsh};
 }
-my $vcf = $patient->getVariationsFileName("deepvariant");
+my $vcf = $patient->getVariationsFileName($type);
 
 my $cmd2 =qq{$singularity $bcftools bcftools view   -c 1  -e '(QUAL<30 && FORMAT/VAF<0.15 && FORMAT/DP < 10)' $vcf_out -o $vcf -O z && $singularity $bcftools bcftools tabix -f -p vcf $vcf};
 print CMD "$cmd && $cmd2 \n";
+}
 }
 close (CMD);
 print "your file is here : \n".$cmd_file."\n";
---
+++ polyscripts/system_utility/run_local.pl
@@ -0,0 +1,68 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use Time::HiRes qw(time);
+use Cwd;
+
+my $file = shift or die "Usage: $0 <fichier_commandes>\n";
+
+die "Fichier non trouvé: $file\n" unless -f $file;
+
+open my $fh, '<', $file or die "Erreur lecture: $!";
+my @commands = grep { /\S/ && !/^#/ } <$fh>;
+close $fh;
+
+chomp @commands;
+
+my $total = scalar @commands;
+die "Aucune commande trouvée\n" unless $total > 0;
+
+my $success = 0;
+my $error = 0;
+my $start_time = time();
+
+open my $log_fh, '>', '/tmp/jobs_errors.log' or die "Erreur log: $!";
+
+
+
+print "=" x 60 . "\n";
+print "? Jobs : $total\n";
+print "=" x 60 . "\n\n";
+
+for my $i (0..$#commands) {
+    my $cmd = $commands[$i];
+    my $count = $i + 1;
+    my $percent = int($count * 100 / $total);
+    my $filled = int($percent / 5);
+    my $bar = "[" . ("=" x $filled) . (" " x (20-$filled)) . "]";
+    
+    my $cmd_display = substr($cmd, 0, 55);
+    $cmd_display .= "..." if length($cmd) > 55;
+    
+    printf "%s %3d%% (%2d/%d) %s", $bar, $percent, $count, $total, $cmd_display;
+    
+    my $cmd_start = time();
+    my $ret = system($cmd . " > /tmp/job_$count.log 2>&1");
+    my $cmd_time = time() - $cmd_start;
+    
+    if ($ret == 0) {
+        $success++;
+        printf " ? (%.1fs)\n", $cmd_time;
+    } else {
+        $error++;
+        printf " ?\n";
+        print $log_fh "JOB $count: ERREUR\nCmd: $cmd\n\n";
+    }
+}
+
+close $log_fh;
+
+my $total_time = time() - $start_time;
+
+print "\n" . "=" x 60 . "\n";
+print "? OK : $success/$total\n";
+print "? ERROR: $error/$total\n";
+print "??  TIME : " . sprintf("%.1f", $total_time) . "s\n";
+print "=" x 60 . "\n";
+
+exit($error > 0 ? 1 : 0);
