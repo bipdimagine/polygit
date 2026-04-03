6221203f1f5e837a395b070314a735a0eff6678d
pnitschke <patrick.nitschke@parisdescartes.fr>
2026-04-03 10:50:59 +0200

    again

--- polypipeline/scripts/scripts_pipeline/dejavu/hg38/variant/duckdb/local_dejavu_rocks_duck.pl
+++ polypipeline/scripts/scripts_pipeline/dejavu/hg38/variant/duckdb/local_dejavu_rocks_duck.pl
@@ -1,35 +1,92 @@
-#!/usr/bin/perl 
+#!/usr/bin/perl
+use CGI qw/:standard :html3/;
+use FindBin qw($Bin); 
 use strict;
-use FindBin qw($Bin);
-use Data::Dumper;
-use RocksDB;
 use Getopt::Long;
-use Carp;
-
-use lib "$Bin/../../../GenBo/lib/obj-nodb/";
-use GBuffer;
-use GenBoNoSqlRocksGenome;
+use List::Util qw(shuffle); 
+use Archive::Tar;
+use File::Path qw(remove_tree); 
+use lib "$Bin/../../../../../../../GenBo/lib/obj-nodb/";
+use lib "$Bin/../../../../../../../GenBo/lib/test/rocks_dejavu/";
 use rocks_dejavu_tests;
+use Data::Dumper;
+use POSIX 'strftime';
+ my @chromosomes = (1..22,'X','Y','MT');
+ my $filetmp =  "/data-beegfs/tmp/cmd.".time;
 
-my ($verbose, $global_check, $old_path, $new_path, $chr_names);
-GetOptions(
-        'old=s' => \$old_path,
-        'new=s' => \$new_path,
-        'chr=s' => \$chr_names,
-        'verbose=s' => \$verbose,
-        'global_check=s' => \$global_check,
-);
 
-my $test_obj = new rocks_dejavu_tests;
-$test_obj->verbose(1) if $verbose;
-$test_obj->global_check() if $global_check;
-$test_obj->set_directory_path_origin($old_path) if ($old_path);
-$test_obj->set_directory_path_new($new_path);
-if ($chr_names) {
-        my @lChr = split(',', $chr_names);
-        $test_obj->{use_chromosomes} = \@lChr;
-}
-my $is_all_ok = $test_obj->launch_test_all_chromosomes();
-confess() if not $is_all_ok;
+
+open(CMD,">$filetmp");
+ my $filename ;
+ my $test_obj;
+ foreach my $version (("HG19","HG38")){
+ 	  $test_obj->{$version} = new rocks_dejavu_tests;
+	  $test_obj->{$version}->release($version); 	
+ 	my $time = time;
+ foreach my $chr (@chromosomes){
+ 		my $date = strftime("%d-%m-%Y", localtime());
+ 		
+ 		my $file = "rocks-".$version."-$date.$time";
+ 		my $dir = $test_obj->{$version}->path_root_prod();
+ 		$filename->{$version} = $file;
+ 		print  "/usr/bin/perl $Bin/dejavu_rocks.pl -chr=$chr -fork=5 -version=$version -file=$file -dir=$dir\n"; 
+	system("/usr/bin/perl $Bin/dejavu_rocks.pl -chr=$chr -fork=5 -version=$version -file=$file -dir=$dir");
+ 	}
+ }
 exit(0);
+ my $run;
+ 
+if ($Bin =~ m|(.*?/polygit)/|) {
+     $run = $1;
+}
+ 
+ system("cd /data-beegfs/tmp/ ; cat $filetmp | $run/polyscripts/system_utility/run_cluster.pl -cpu=20 -limit=20 -noprint=1 && touch $filetmp.ok");
+ die("problem ") unless "$filetmp.ok";
 
+warn "end cluster ok next ";
+ my @chrs = (1..22, 'X', 'Y','MT' );
+ #die() unless -e "$filetmp.ok";
+ 	foreach my $version (("HG19","HG38")){
+ 	 	my $dir_final = $test_obj->{$version}->path_root_prod."/".$filename->{$version};
+ 	 	warn $dir_final;
+		$test_obj->{$version}->set_directory_path_new($dir_final);
+		foreach my $chr (@chrs){
+			warn "START ".$chr; 
+			$test_obj->{$version}->{use_chromosomes} = [$chr];
+			my $is_all_ok = $test_obj->{$version}->launch_test_all_chromosomes();
+			die("problem ") if not $is_all_ok;
+		}
+	}
+foreach my $version (("HG19","HG38")){
+	my $dir = $test_obj->{$version}->path_root_prod()."/".$filename->{$version} ;
+	system("$Bin/list_project_parquet.pl -dir=$dir");
+}
+foreach my $version (("HG19","HG38")){
+my $prod_path = $test_obj->{$version}->path_origin;
+my $dir_final = $test_obj->{$version}->path_root_prod."/".$filename->{$version};
+if ( -l $prod_path ) {  # -l = is symlink
+ 	my $real = $test_obj->{$version}->path_real();
+ 	my @t = split("/",$real);
+ 	my $name = pop(@t);
+ 	my $dir = join("/",@t);
+ 	my $tar = $real.".tar";
+ 	system("tar -cvf $tar -C $dir $name > /dev/null 2>&1");
+    unlink $prod_path or warn "cannot unlink $prod_path: $!";
+    system("ln -s $dir_final $prod_path");
+    unlink $prod_path.".phenotype" or warn "cannot unlink $prod_path: $!";
+     system("ln -s $dir_final.phenotype $prod_path.phenotype");
+    system("zstd -T20 --rm $real.tar && rm -rf $real");
+}
+else {
+	confess();
+}
+}
+
+exit(0);
+#
+#foreach my $version (("HG19","HG38")){
+#my $backup_path = ;
+#my $file = $test_obj->path_root_prod."/variations/rocks.backup.tar.gz";
+#system ("tar -zcvf $file -C $backup_path rocks.backup");
+#}
+ 	
