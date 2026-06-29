c82ce1e038c779f47fbc3e06e587e942f4409644
pnitschke <patrick.nitschke@parisdescartes.fr>
2026-06-29 14:46:24 +0200

    Merge branch 'duckdb' of https://github.com/bipdimagine/polygit.git into duckdb

--- polypipeline/scripts/scripts_pipeline/manue_cnv/cnv/filter_cnv.pl
+++ polypipeline/scripts/scripts_pipeline/manue_cnv/cnv/filter_cnv.pl
@@ -89,7 +89,6 @@ my $tabix_blacklist = Bio::DB::HTS::Tabix->new( filename => "/data-pure/public-d
 my $nb = 0;
 foreach my $row (@$array_ref) {
 	$nb++;
-	warn $nb."/".scalar(@$array_ref) if $nb %10 ==0;
 	 my $cnv = $rocks->get($row->{id});
 	  my @t =  keys %{$cnv->{score}};
 	 #next  if @t > 1;
@@ -125,11 +124,12 @@ foreach my $row (@$array_ref) {
 		
 		my $na =0 ;
 		 warn $l if $start == 177950001;
-		if ($l > 10000){
-			my $v  = analyze_cnv_noise($patient,$cnv);
+		if ($l > 5000 && $patient->isRevio){
+			my ($v,$break,$delta)  = analyze_cnv_noise($patient,$cnv);
 		
-			$row->{blacklist} = 70 if $v > 30;
-				$row->{blacklist} = 80 if $v > 40;
+			$row->{blacklist} = 70 if $v > 30 && $break > 20;
+			$row->{blacklist} = 80 if( $v > 40 && $break > 30) or $delta > 50 ;
+			#	warn Dumper $v;
 			
 		}
 		my $res = $tabix->query( $region );
@@ -204,9 +204,9 @@ sub test_type {
 }
 
 sub analyze_cnv_noise {
-    my ($patient, $cnv,) = @_;
+    my ($patient, $cnv) = @_;
     
-    my $bin_size = 300;
+    my $bin_size = 100;
     my $chrom = $project->getChromosome($cnv->{chromosome})->fasta_name;
     my $pos_start = $cnv->{start};
     my $pos_end = $cnv->{end};
@@ -221,30 +221,37 @@ sub analyze_cnv_noise {
 
     # 2. Lancer la commande UCSC bigWigSummary
     # Rediriger STDERR vers /dev/null pour cacher les warnings de l'UCSC
-    my $cmd = "/software/bin/bigWigSummary $bw $chrom $pos_start $pos_end $num_bins 2>/dev/null";
-    my $output = `$cmd`;
-    chomp $output;
+    my @values;
+    for (my $i =$pos_start;$i <= $pos_end-$bin_size;$i+=$bin_size){
+    $patient->depth($chrom,$i,$i+$bin_size);
+    	push(@values,$patient->meanDepth($chrom,$i,$i+($bin_size*2)));
+    }
+    
+    my $mean1 = $patient->meanDepth($chrom,$pos_start-5000,$pos_start-200);
+     my $mean2 = $patient->meanDepth($chrom,$pos_end+200,$pos_end+5000);
+    #my $cmd = "/software/bin/bigWigSummary $bw $chrom $pos_start $pos_end $num_bins 2>/dev/null";
+    #my $output = `$cmd`;
+    #chomp $output;
 
     # Si la commande échoue ou ne renvoie rien
-    if (!$output) {
-        return (0, 0, 0, "ERREUR_LECTURE (ou région sans aucune donnée)");
-    }
+    #if (!$output) {
+     #   return (0, 0, 0, "ERREUR_LECTURE (ou région sans aucune donnée)");
+    #}
 
     # 3. Récupérer les valeurs (elles sont séparées par des tabulations)
-    my @values = split(/\t/, $output);
+    #my @values = split(/\t/, $output);
 
     # 4. Nettoyer les données : bigWigSummary renvoie "n/a" s'il y a un trou absolu (0 reads)
-    for my $val (@values) {
-        if ($val eq 'n/a') {
-            $val = 0;
-        }
-    }
+   # for my $val (@values) {
+    #    if ($val eq 'n/a') {
+     #       $val = 0;
+      #  }
+    #}
 
     # 5. Calculs statistiques
     my $n = scalar(@values);
     my $sum = 0;
     $sum += $_ for @values;
-    
     my $mean = $sum / $n;
 
     # Si la moyenne est à 0 (aucun read du tout sur tout le CNV)
@@ -262,8 +269,114 @@ sub analyze_cnv_noise {
 
     # Calcul du Coefficient de Variation (en pourcentage)
     my $cv = ($stddev / $mean) * 100;
-    warn $cv." ".$mean if $pos_start == 55675001;
-	return ($mean,$cv);
+   my $noise = 0;
+
+for (my $i = 1; $i < @values; $i++) {
+
+    $noise += abs($values[$i] - $values[$i-1]);
+
+}
+
+$noise /= (@values-1);
+my $threshold = 0.20;   # 20 %
+
+my $count = 0;
+
+for (my $i=1;$i<@values;$i++){
+
+    my $local = ($values[$i]+$values[$i-1])/2;
+
+    next if $local == 0;
+
+    my $delta = abs($values[$i]-$values[$i-1])/$local;
+
+    $count++ if $delta > $threshold;
+
+}
+
+my $fraction = $count/(@values-1);
+
+	my $r = plateau_breaks(\@values,3,0.20);
+
+
+my $nbbp = $r->{nb_breaks};
+	my $pb = ($r->{nb_breaks}/(@values))*100;
+	my $outside_delta = abs($mean1-$mean2)/(($mean1+$mean2)/2);
+    warn $cv." - noide: $noise - $pb - ".$mean." ".$mean1." ".$mean2." : ".($outside_delta*100);# if $pos_start == 55675001;
+  #  warn Dumper @values if $pb> 20;
+    
+  #  die()   if $noise > 0.3;
+   # die();
+	return ($mean,$cv,$pb,$outside_delta);
 	
+}
+
+
+sub plateau_breaks {
+
+    my ($values,$window,$threshold) = @_;
+
+     $window    //= 4;      # taille des fenêtres
+
+     $threshold //= 0.25;   # 25%
+
+    my @v = @$values;
+
+    my $nb_breaks = 0;
+
+    my @breaks;
+
+    for (my $i=$window;$i<@v-$window;$i++) {
+
+        my ($left,$right) = (0,0);
+
+        for(my $j=1;$j<=$window;$j++) {
+
+            $left  += $v[$i-$j];
+
+            $right += $v[$i+$j];
+
+        }
+
+        $left  /= $window;
+
+        $right /= $window;
+
+        next if $left==0 && $right==0;
+
+        my $ref = ($left+$right)/2;
+
+        next if $ref==0;
+
+        my $delta = abs($left-$right)/$ref;
+		
+        if($delta > $threshold){
+
+            push @breaks,{
+
+                pos   => $i,
+
+                left  => $left,
+
+                right => $right,
+
+                delta => $delta,
+
+            };
+
+            $nb_breaks++;
+
+        }
+
+    }
+
+    return {
+
+        nb_breaks => $nb_breaks,
+
+        breaks    => \@breaks,
+
+    };
+
 }
 
