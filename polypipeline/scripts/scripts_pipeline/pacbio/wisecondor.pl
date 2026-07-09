df205e22cc9f1452f30e7563bb7017d85ede919b
pnitschke <patrick.nitschke@parisdescartes.fr>
2026-07-09 16:19:43 +0200

    suist epipeline revio

---
+++ polypipeline/scripts/scripts_pipeline/pacbio/wisecondor.pl
@@ -0,0 +1,60 @@
+#!/usr/bin/env perl
+use FindBin qw($Bin);
+use strict;
+use lib "$Bin/../../../GenBo/lib/obj-nodb/";
+use lib "$Bin/../../packages/";
+#use Set::IntSpan;
+use GBuffer; 
+use Data::Dumper;
+use Getopt::Long;
+use autodie qw(system);
+
+
+ my $project_name;
+ my $fork;
+ my $callable_intspan_file;
+ my $patient_name;
+ #my $low_calling;
+ 
+my $force;
+ 
+ my $fork = 5;
+GetOptions(
+	'project=s'   => \$project_name,
+	"patient=s" => \$patient_name,
+	"fork=s" => \$fork,
+	"force=s" =>\$force,
+);
+die("miss fork") unless $fork;
+
+warn "1";
+my $buffer = GBuffer->new();
+my $project = $buffer->newProject( -name => $project_name );
+my $wise_sif  = "wisecondor.sif";
+my $singularity = "run_singularity.sh";
+my $patient = $project->getPatient($patient_name);
+ my $bam = $patient->getBamFile() ;
+ my $ref;
+ if ($bam =~ /\.cram/ ) {
+ 	$ref = "--reference ".$project->genomeFasta();
+ }
+ 
+ my $chr = $project->getChromosome("1");
+ my $fileout = $patient->fileWiseCondor();
+ my $fileout_wise = $fileout;
+ $fileout_wise =~ s/\.npz//;
+ if (-e $fileout){
+ 	if ($force){
+ 		unlink $fileout;
+ 	}
+ 	else {
+ 		exit(0);
+ 	}
+ }
+ my $cmd = "exec_singularity.sh wisecondor.sif  wisecondorx convert $bam $fileout_wise $ref";
+ warn $cmd;
+ system("$cmd");
+ 
+ die() unless -e $fileout;
+
+ 
