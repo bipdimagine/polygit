d3022820ccb4105bb056459e61c93215ea51a38e
mperin <mperin@sanger.cm.cluster>
2026-08-12 11:26:43 +0200

    sawfish

--- polypipeline/scripts/scripts_pipeline/pacbio/sawfish.pl
+++ polypipeline/scripts/scripts_pipeline/pacbio/sawfish.pl
@@ -1,10 +1,8 @@
 #! /usr/bin/env perl
 use FindBin qw($Bin);
 use strict;
-
 use lib "$Bin/../../../../GenBo/lib/obj-nodb/";
 use lib "$Bin/../../packages/";
-#use Set::IntSpan;
 use GBuffer; 
 use Data::Dumper;
 use Getopt::Long;
@@ -15,42 +13,10 @@ use autodie qw(system);
 
 
 
- my $project_name;
- my $fork;
- my $callable_intspan_file;
- my $patient_name;
- #my $low_calling;
- my $method;
- 
- my $chr_syno ={
-		1=> "chr1",
-		2=>"chr2",
-		3=>"chr3",
-		4=>"chr4",
-		5=>"chr5",
-		6=>"chr6",
-		7=>"chr7",
-		8=>"chr8",
-		9=>"chr9",
-		10=>"chr10",
-		11=>"chr11",
-		12=>"chr12",
-		13=>"chr13",
-		14=>"chr14",
-		15=>"chr15",
-		16=>"chr16",
-		17=>"chr17",
-		18=>"chr18",
-		19=>"chr19",
-		20=>"chr20",
-		21=>"chr21",
-		22=>"chr22",
-		X=>"chrX",
-		Y=>"chrY",
-		MT=>"chrM",
-};
-
- 
+my $project_name;
+my $callable_intspan_file;
+my $patient_name;
+my $method;
 my $fork = 5;
 my $force;
 
@@ -77,8 +43,8 @@ my $patient = $project->getPatient($patient_name);
 my $run = $patient->getRun();
 my $ref =  $project->genomeFasta();
 my $dir_out= $project->getAlignmentPipelineDir($patient->name);
-my $bam_out = $dir_out."/".$patient->name.".bam";
-die() unless -e $bam_out;
+my $bam = $dir_out."/".$patient->name.".bam";
+$bam = $patient->getAlignmentFile unless -e $bam;
 
 
 my $dir_pipeline = $dir_out."/sawfiwh1".time;
@@ -94,18 +60,19 @@ if ($patient->isMale) {
 }
 my $vcf = $dir_out."/".$patient->name.".vcf.gz" ;
  $vcf = $patient->vcfFileName("deepvariant") unless -e $vcf;
-my $prod_file = $dir_out."/".$patient->name.".vcf.gz";
+my $prod_file = $patient->vcfFileName("sawfish");
 if (-e $prod_file){
 	if ($force){
 		unlink $prod_file;
 		unlink $prod_file.".tbi";
 	}
 	else {
-		warn "already done";
+		warn "already done: $prod_file";
 		exit(0);
 	}
 }
-my $cmd = qq{$singularity $sawfish sawfish discover --threads $fork --ref $ref  --bam $bam_out  --expected-cn $expected --cnv-excluded-regions $excluded --output-dir $dir_pipeline --maf-sample-name $vcf };
+
+my $cmd = qq{$singularity $sawfish sawfish discover --threads $fork --ref $ref  --bam $bam  --expected-cn $expected --cnv-excluded-regions $excluded --output-dir $dir_pipeline --maf-sample-name $vcf };
 
 my $cmd2 = qq{$singularity $sawfish sawfish joint-call --threads $fork --sample $dir_pipeline --output-dir $dir_opt2 };
 
@@ -117,7 +84,7 @@ warn "$dir_opt2/genotyped.sv.vcf.gz";
 system("$cmd && $cmd2" ) unless -e "$dir_opt2/genotyped.sv.vcf.gz";
 #die();
 my $prod_file = $patient->vcfFileName("sawfish");# unless -e $vcf;
-my $cmd3 = "cp $dir_opt2/genotyped.sv.vcf.gz  $prod_file && tabix -p vcf $prod_file";
+my $cmd3 = "cp $dir_opt2/genotyped.sv.vcf.gz $prod_file && tabix -p vcf $prod_file";
 system($cmd3);
 die() unless -e $prod_file.".tbi";
 
